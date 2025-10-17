6c0f886a9bbe192de2a24e30803fa64b022d22f4
Bennett Clark <143957700+Waffler19@users.noreply.github.com>
2025-10-16 22:57:05 -0400

    Refactor comments for clarity in example.py

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
-from Basis import Basis
-k=Basis('2pz', number_of_gaussians=3, p_thresh=0.01, plot_type='Radial', r_inc=0.05, rmax=6)
-k.kernel()
+from Basis import Basis #importing the Basis Class
+k=Basis('2pz', number_of_gaussians=3, p_thresh=0.01, plot_type='Radial', r_inc=0.05, rmax=6) #generate a basis object with desired params for 2pz orbital
+k.kernel() #run basis calculations 
